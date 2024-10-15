4aa6f359ae28f23ff94d3f74da376fbd53b42e5f
blayne-wesneski <102747184+blayne-wesneski@users.noreply.github.com>
2024-10-15 18:25:48 -0400

    Create generateMaze.java  - define bounds of the maze using a 2D array  - placeholder TODO comments to prepare for algorithm

---
+++ src/generateMaze.java
@@ -0,0 +1,12 @@
+public class generateMaze {
+    int[][] maze = new int[5][5];
+
+    // @TODO: Make a hashmap to keep track of empty/obstacle/wall cells
+    // For now 0 = empty, 1 = obstacle 2 = wall.
+
+    public generateMaze() {
+        //@TODO: generate the bounds of the maze
+
+        //@TODO: generate the obstacles in the maze randomly
+    }
+}
