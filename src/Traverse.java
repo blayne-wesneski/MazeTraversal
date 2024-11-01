70918fdb75db0613a0c77a765f0d887411852d6a
blayne-wesneski <102747184+blayne-wesneski@users.noreply.github.com>
2024-11-01 00:46:24 -0400

    add comments

--- src/Traverse.java
+++ src/Traverse.java
@@ -35,17 +35,26 @@ public class Traverse {
         // Add the starting point to the queue
         queue.add(maze[startRow][startCol]);
 
+        // Scanner for testing
         Scanner scanner = new Scanner(System.in);
+        
+        // While the queue isn't empty, keep searching
+        // This does currently just end if it can't find the exit, I will write an else statement for that later.
         while (!queue.isEmpty()) {
             Point curr = queue.poll();
 
+            // Search all 4 directions at every point
             for (int i = 0; i < directions.length; i++) {
 
                 int nextRow = curr.row + directions[i][0];
                 int nextCol = curr.col + directions[i][1];
 
+
                 if (nextRow >= 0 && nextRow < maze.length && nextCol >= 0 && nextCol < maze[0].length) {
+                    // Queue up next point
                     Point next = maze[nextRow][nextCol];
+                    
+                    // If the point is the end, we are done, print it out.
                     if (next.isEnd()) {
                         maze[nextRow][nextCol].distance = curr.distance + 1;
                         maze[nextRow][nextCol].parent = curr;
@@ -55,6 +64,8 @@ public class Traverse {
                         scanner.close();
                         return;
                     }
+
+                    // if it isn't the end, check for walkability, if it isn't walkable, then ignore it.
                     if (next.isWalkable()) {
                         System.out.println("Visiting row: " + next.row + " col: " + next.col);
                         maze[next.row][next.col].type = '▦';
@@ -82,18 +93,33 @@ public class Traverse {
 
     }
 
+    /**
+     * Backtracks through previous points to print final shortest path.
+     * @param point end point found with search algorithm
+     * @param maze the maze array
+     */
     private void printFinalPath(Point point, Point[][] maze){
+        
+        // Create an arraylist to keep track of the previous points
         List<Point> path = new ArrayList<>();
         
+        // While the point has a parent, keep going back
         while(point.parent != null){
             path.add(point);
             point = point.parent;
         }
+
         Collections.reverse(path);
+
+        // For each point in the path list, update the maze array
         for (Point point2 : path) {
+
+            // Don't overwrite the 'E'
             if(maze[point2.row][point2.col].type != 'E'){
                 maze[point2.row][point2.col].type = '▢';
             }
+
+            // Cleanup code, just removes the search history ;-)
             for (int i = 1; i < maze.length - 1; i++){
                 for (int j = 1; j < maze.length - 1; j++) {
                     if(maze[i][j].type == '▦'){
